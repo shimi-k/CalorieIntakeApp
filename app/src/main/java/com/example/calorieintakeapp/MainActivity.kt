56cffc5e73df367d78f255885b770d1d457c3075
K.Shimizu_2 <shimik1212@gmail.com>
2020-06-20 23:27:25 +0900

    メイン画面がでるようにしたよ フラグメントの画面は実装中だよ

--- app/src/main/java/com/example/calorieintakeapp/MainActivity.kt
+++ app/src/main/java/com/example/calorieintakeapp/MainActivity.kt
@@ -2,14 +2,26 @@ package com.example.calorieintakeapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    val TAG = MainFragment::class.java.name
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
 
+        Log.d(TAG, "in onCreate")
+
+
         supportFragmentManager.beginTransaction()
-            .add(android.R.id.content, MainFragment())
+            .replace(android.R.id.content, MainFragment())
+            .addToBackStack(MainFragment::class.java.simpleName)
+            .commitAllowingStateLoss()
+
+        Log.d(TAG, "out onCreate")
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
